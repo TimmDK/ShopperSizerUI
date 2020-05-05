17099aaebc6efb257554fe53cf63a3b58aac4697
jaco6002 <jaco6002@edu.easj.dk>
2020-05-05 13:30:42 +0200

    added typescript

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,4 +3,38 @@ import axios, {
     AxiosError
 } from "../../node_modules/axios/index";
 
+interface ILiveNumber{
+    id:number;
+    number:number;
+}
+
+let baseurl = "https://shoppersizerrest.azurewebsites.net/api/LiveNumber/1"
+
+new Vue({
+    el: "#app",
+    data:{
+        LiveNumber: 0,
+        remaining:0,
+        maxvalue: 200,
+    },
+    methods:{
+        getLiveNumber(){
+            axios.get<ILiveNumber>(baseurl)
+            .then((response: AxiosResponse<ILiveNumber>)=>{
+                this.LiveNumber = response.data.number
+                this.getRemainingValue()
+                console.log(response.data)
+            })
+            .catch((error: AxiosError)=>{
+                this.errorMessage = error.message
+            })
+        },
+        getRemainingValue(){
+            this.remaining = this.maxvalue - this.LiveNumber
+        },
+    },
+    created(){
+        this.getLiveNumber()
+    },
+})
 
